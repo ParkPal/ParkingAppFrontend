acd89a75810c82156df4a5955edf047022fed89b
benmxavier <benmxavier@gmail.com>
2018-04-12 12:10:56 -0400

    History changable from admin dash

--- django_files/polls/admin.py
+++ django_files/polls/admin.py
@@ -3,6 +3,8 @@ from django.contrib import admin
 # Register your models here.
 
 
-from .models import Question
+from .models import Question, Host, Node
 
 admin.site.register(Question)
+admin.site.register(Host)
+admin.site.register(Node)
