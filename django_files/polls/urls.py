53f437e7ab739d13b809f37ad1507ab3cc93fabe
benmxavier <benmxavier@gmail.com>
2018-04-12 14:05:17 -0400

    Modified Url paths

--- django_files/polls/urls.py
+++ django_files/polls/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     path('<int:question_id>/vote/', views.vote, name='vote'),
     #path(r'', views.DashView.as_view(), name='dash'),
     path('/pubdash/', views.DashView.as_view(), name='lot_dash'),
-    path('<user_username>/dash/', views.OwnerDashView.as_view(), name = 'owner_dash')
-    path('<user_username>/<host_lotName>/lot_report', polls.views.lot_graph_view, name = 'lot_graph_view')
+    path('<user_username>/dash/', views.OwnerDashView.as_view(), name = 'owner_dash'),
+    path('<user_username>/<host_lotName>/lot_report', polls.views.lot_graph_view, name = 'lot_graph_view'),
     path('<user_username>/<host_lotName>/dash', views.LotDashView.as_view(), name = 'lot_dash')
 ]
